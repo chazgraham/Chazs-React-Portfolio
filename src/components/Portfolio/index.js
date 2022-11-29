b4c6c8dcecb24e3b22663db505e7f02d76720869
chaz Graham <chazhg80.cg@gmail.com>
2022-11-29 15:17:46 -0600

    rendered project images

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function Portfolio() {
 
+    const [photos] = useState([
+        {
+          name: 'Grocery aisle',
+          category: 'commercial',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+        },
+        {
+          name: 'Grocery booth',
+          category: 'commercial',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+        },
+        {
+          name: 'Building exterior',
+          category: 'commercial',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+        },
+    ]);
   return (
-    <section>
-      <h1>NAME</h1>
-      <p>DESCRIPTION</p>
-    </section>
+    <div>
+      <div className="flex-row">
+        {photos.map((image, i) => (
+          <img
+            src={require(`../../assets/projectImg/${i}.jpg`)}
+            alt={image.name}
+            className="img-thumbnail mx-1"
+            key={image.name}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
 export default Portfolio;
