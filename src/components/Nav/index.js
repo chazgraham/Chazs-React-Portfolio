524686b77bed9acb404a2788c28b66a98ad16bc9
chaz Graham <chazhg80.cg@gmail.com>
2023-01-21 15:41:36 -0600

    restyled navbar and about page

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,35 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
 
 function AppNavbar() {
 
   return (
     <>
-      <Navbar variant='dark' expand='lg'>
-        <Container fluid>
-          <Navbar.Brand as={Link} to='/'>
-            <h1>Chaz Graham</h1>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls='navbar' />
-          <Navbar.Collapse id='navbar'>
-            <Nav className='ml-auto'>
-              <Nav.Link as={Link} to='/'>
-                About Me
-              </Nav.Link>
-              <Nav.Link as={Link} to='portfolio'>
-                Portfolio
-              </Nav.Link>
-              <Nav.Link as={Link} to='contact'>
-                Contact
-              </Nav.Link>
-              <Nav.Link as={Link} to='resume'>
-                Resume
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <div className='navbar-con'>
+        <Nav className="justify-content-center">
+          <Nav.Link as={Link} to='/'>
+            About Me
+          </Nav.Link>
+          <Nav.Link as={Link} to='portfolio'>
+            Portfolio
+          </Nav.Link>
+          <Nav.Link as={Link} to='contact'>
+            Contact
+          </Nav.Link>
+          <Nav.Link as={Link} to='resume'>
+            Resume
+          </Nav.Link>
+        </Nav>
+      </div>
     </>
   );
 }
