aebbb81f7d2ea62edf98b1283982b8a0f12de1ca
chaz Graham <chazhg80.cg@gmail.com>
2023-03-17 16:22:28 -0500

    added fixed resume download btn

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -10,7 +10,7 @@ function Resume() {
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
       <div>
       <h2>Resume</h2>
-      Download my<a href='resume.pdf' download="resume.pdf">
+      Download my<a href='/resume.pdf' download="resume.pdf">
         <button className='resume-btn'><span class="material-symbols-outlined">description</span>Resume</button>
         </a>
       </div>
