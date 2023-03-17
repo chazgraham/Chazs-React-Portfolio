a94abec0709124870a419b2884b644dd9f8900ca
chaz Graham <chazhg80.cg@gmail.com>
2023-03-17 16:35:56 -0500

    added resume import

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import resumeImg from '../../assets/profileImg/resume-img.jpg';
 import resumeImg2 from '../../assets/profileImg/resume-img2.jpg';
+import reume from '../../assets/resume/resume.pdf';
 
 function Resume() {
 
@@ -10,7 +11,7 @@ function Resume() {
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
       <div>
       <h2>Resume</h2>
-      Download my<a href='/resume.pdf' download="resume.pdf">
+      Download my<a href={reume} download="resume.pdf">
         <button className='resume-btn'><span class="material-symbols-outlined">description</span>Resume</button>
         </a>
       </div>
