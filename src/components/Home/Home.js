c73d55b4b15724012fc4358a3a44058acbeb5eb4
chaz Graham <chazhg80.cg@gmail.com>
2023-04-14 21:21:37 -0500

    added soft skills and hover color to footer

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,8 +10,8 @@ function Home() {
                 <div className="home-content">
                     <h2>Full-Stack Web Developer</h2>
                     <p className="home-text">Hi I'm Chaz Graham. A passionate Full-stack web developer based out of San Antonio, TX</p>
-                    <p className="home-skills">Technical Skills: JavaScript, React, MongoDb, HTML, Express, GraphQl</p>
-                    <p className="home-skills">Soft Skills: JavaScript, React, MongoDb, HTML, Express, GraphQl</p>
+                    <p className="home-skills">Technical Skills: JavaScript, React, MongoDb, HTML, Express, GraphQl, Bootstrap.</p>
+                    <p className="home-skills">Soft Skills: Communication, Teamwork, Problem-solving, Flexibility, Adaptability.</p>
                 </div>
                 <div className='profile-info'>
                     <img className='profile-img' src={profileImg} alt="chaz Graham" />
