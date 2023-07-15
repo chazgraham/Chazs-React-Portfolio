16c4fd841e018d4fe4d925d263d5401a927caed5
chaz Graham <chazhg80.cg@gmail.com>
2023-07-15 00:56:01 -0500

    styled the first section in home, moved the background img to the first section background and removed it from body

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,11 +6,15 @@ function Home() {
     return (
         <>
             <section className="img_container">
-                    <div className="centered">
-                        <h1>I am Chaz Graham</h1>
-                        <h4>A passionte Full-stack web developer</h4>
-                        <button>Resume</button>
-                    </div>
+                <div className="centered">
+                    <h1>I am Chaz Graham</h1>
+                    <h2>A passionte Full-stack web developer</h2>
+                    <button className="resume_btn">Resume</button>
+                </div>
+            </section>
+            <section className="about">
+                <h3>About me</h3>
+                <p>I am a former medic for the United States Air Force, I served for six years before separating to further my education with my newly acquired benefits. I went into the nursing profession to further build the medical skills I already possessed. Two semesters into my degree I found I really did not like nursing or practicing medicine as a whole. After searching for careers on the web I found the profession of software engineer. After being told I was tech savvy by my friends and family I decided to change the course of my career. During my time coding I find myself more engaged and have a heightened drive to continue learning. Instead of dreading going to work I wake up with drive to finish my projects and solve the problems that sit before me.</p>
             </section>
         </>
     )
