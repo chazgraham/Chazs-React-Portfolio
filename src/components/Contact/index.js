a349447a548ba03ffd87b59ea20d5eaba9457d26
chaz Graham <chazhg80.cg@gmail.com>
2023-07-16 14:53:06 -0500

    removed old contact code and replace with new structure

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,25 +1,40 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
 
 function ContactForm() {
 
   return (
-    <Container className='contact'>
-      <aside>
-        <i class="fa fa-solid fa-envelope fa-5x"></i>
-        <h5>Address and Phone</h5>
-        <p>
-          Chaz Graham
-          <br></br> 
-          San Antionio, TX
-          <br></br>
-          210-215-1351 
-        </p>
-      </aside>
-      <div className='email'>
-      <p>Please feel free to get in touch with me if you have any questions or concerns by emailing me at <a href='mailto: chazhg80.cg@gmail.com'>chazhg80.cg@gmail.com</a></p>
-      </div>
-    </Container>
+    <>
+      <section className='contact_img'>
+        <div>
+          <h1>CONTACT ME</h1>
+        </div>
+      </section>
+      <section>
+        <div>
+          <h2>Location:</h2>
+          <div>Map goes here</div>
+        </div>
+        <div>
+          <h2>Talk To Me</h2>
+          <div>
+            <h5>EMAIL</h5>
+            <a href = "mailto: chazhg80.cg@gmail.com">chazhg80.cg@gmail.com</a>
+            <h5>Follow Me:</h5>
+            <ul className='flex-row'>
+                <li>
+                    <a href='https://github.com/chazgraham'><i class="fa fa-github fa-2x"></i></a>
+                </li>
+                <li>
+                    <a href='https://www.linkedin.com/in/chaz-graham-a95a43258'><i class="fa fa-linkedin-square fa-2x"></i></a>
+                </li>
+                <li>
+                    <a href='https://stackoverflow.com/users/19429367/chaz-graham'><i class="fa fa-stack-overflow fa-2x"></i></a>
+                </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
