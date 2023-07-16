139c7554ec7007e1f4d7fc9fb4ed45cfe7771140
chaz Graham <chazhg80.cg@gmail.com>
2023-07-16 00:22:42 -0500

    restructured the nav links and restyled them

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -11,14 +11,14 @@ function AppNavbar() {
           <Navbar.Toggle aria-controls='responsive-navbar-nav' />
           <Navbar.Collapse id='responsive-navbar-nav'>
             <Nav className='ml-auto'>
-              <Nav.Link as={Link} to='portfolio'>
+              <Nav.Link as={Link} to='Chazs-React-Portfolio'>
                 Home
-              </Nav.Link>
-              <Nav.Link as={Link} to='contact'>
-                Contact
               </Nav.Link>
               <Nav.Link as={Link} to='resume'>
                 Resume
+              </Nav.Link>
+              <Nav.Link as={Link} to='contact'>
+                Contact
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
