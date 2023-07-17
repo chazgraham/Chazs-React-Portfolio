cb0f003a330aaa128df63382b671a9a4a063556b
chaz Graham <chazhg80.cg@gmail.com>
2023-07-17 18:54:03 -0500

    made contact page moblie responsive

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -12,7 +12,7 @@ function ContactForm() {
       <section className='info_section'>
         <div>
           <h2 className='contact_h2' >Location:</h2>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d111224.02025107306!2d-98.54806078174981!3d29.40685012635872!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x865c58af04d00eaf%3A0x856e13b10a016bc!2sSan%20Antonio%2C%20TX!5e0!3m2!1sen!2sus!4v1689538051749!5m2!1sen!2sus" width="600" height="450" style={{border:0}} allowfullscreen="?" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title='my city'></iframe>
+          <iframe className='map_container' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d111224.02025107306!2d-98.54806078174981!3d29.40685012635872!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x865c58af04d00eaf%3A0x856e13b10a016bc!2sSan%20Antonio%2C%20TX!5e0!3m2!1sen!2sus!4v1689538051749!5m2!1sen!2sus" width="600" height="450" style={{border:0}} allowfullscreen="?" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title='my city'></iframe>
         </div>
         <div>
           <h2 className='contact_h2'>Talk To Me:</h2>
