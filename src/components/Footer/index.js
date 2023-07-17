77ccc9f52824e31ed3558f8d960ed9570560c01e
chaz Graham <chazhg80.cg@gmail.com>
2023-07-17 18:25:16 -0500

    added onClick handlers to my bottom nav to send to top of page

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,17 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Footer () {
+    const sendToTop = (location) => {
+        if (window.location === location) {
+            window.scrollTo(0, 0);
+        } else {
+            return
+        }
+    }
+
     return (
         <footer>
             <ul className='footer_ul'>
                 <li className='footer_li'>
-                    <Link to='/Chazs-React-Portfolio'>Home</Link>
+                    <Link to='/Chazs-React-Portfolio' onClick={() => sendToTop(window.location)}>Home</Link>
                 </li>
                 <li className='footer_li'>
-                    <Link to='/resume'>Resume</Link>
+                    <Link to='/resume' onClick={() => sendToTop(window.location)}>Resume</Link>
                 </li>
                 <li className='footer_li'>
-                    <Link to='/contact'>Contact</Link>
+                    <Link to='/contact' onClick={() => sendToTop(window.location)}>Contact</Link>
                 </li>
             </ul>
             <p className='footer_p'>©2022 Chaz Graham portfolio. All right reserved.</p>
