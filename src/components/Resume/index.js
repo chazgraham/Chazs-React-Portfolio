5935ec824f6257ec683d7d34e753f617be643877
chaz Graham <chazhg80.cg@gmail.com>
2023-07-21 19:49:52 -0500

    replace all foodies content for arcadium

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -21,8 +21,8 @@ function Resume() {
             <p className='resume_p_title'>San Antonio, TX</p>
             <p className='resume_p_title'>Email: <a href="mailto: chazhg80.cg@gmail.com" className='resume_link'>chazhg80.cg@gmail.com</a></p>
             <p className='resume_p_title'>Linkedin: <a href='https://www.linkedin.com/in/chaz-graham-a95a43258' className='resume_link'>www.linkedin.com/in/chaz-graham-a95a43258</a></p>
-            <p className='resume_p_title'>GitHub: <a href='https://github.com/chazgrahamChaz' className='resume_link'>https://github.com/chazgrahamChaz</a></p>
             <p className='resume_p_title'>Portfolio: <a href='http://chazgraham.github.io/Chazs-React-Portfolio' className='resume_link'>http://chazgraham.github.io/Chazs-React-Portfolio</a></p>
+            <p className='resume_p_title'>GitHub: <a href='https://github.com/chazgrahamChaz' className='resume_link'>https://github.com/chazgrahamChaz</a></p>
           </div>
           <div>
             <h3>Summary</h3>
@@ -38,7 +38,15 @@ function Resume() {
         <section>
           <h3>Relative Experience</h3>
           <div>
-            <p className='resume_p_title'>Game Seeker: <a href='https://github.com/chazgraham/Game-Seeker'>https://github.com/chazgraham/Game-Seeker</a> | <a href='https://game-seeker.herokuapp.com/'>https://game-seeker.herokuapp.com/</a></p>
+            <p className='resume_p_title'>Arcadium: <a href='https://github.com/chazgraham/Arcadium' className='resume_link'>GitHub Repository</a> | <a href='http://chazgraham.github.io/Arcadium' className='resume_link'>Deployed Application</a></p>
+            <ul>
+              <li>An application that allows the user to choose a game that is posted on the homepage and allow them to play the game and view their score. All logic coded in JavaScript.</li>
+              <li>This application cures boredom and allows the user to relax by playing simple games.</li>
+              <li>Technologies Used: React, JavaScript, CSS</li>
+            </ul>
+          </div>
+          <div>
+            <p className='resume_p_title'>Game Seeker: <a href='https://github.com/chazgraham/Game-Seeker' className='resume_link'>GitHub Repository</a> | <a href='https://game-seeker.herokuapp.com/' className='resume_link'>Deployed Application</a></p>
             <ul>
               <li>The application allows users to track games they have played, are playing, and have completed. It also allows the user to make friends and view the same list on their profiles.</li>
               <li>I was fully responsible for all aspects of the application down to styling and database creation as well as API calls from front-end to back-end. Used effective time management skills to meet my deadline.</li>
@@ -46,15 +54,7 @@ function Resume() {
             </ul>
           </div>
           <div>
-            <p className='resume_p_title'>Foodies: <a href='https://github.com/chazgraham/Foodies'>https://github.com/chazgraham/Foodies</a> | <a href='http://chazgraham.github.io/Foodies'>http://chazgraham.github.io/Foodies</a></p>
-            <ul>
-              <li>An application that allows the user to search for food recipes using the third party spoonacular API. The application will have a short summary as well as steps to complete the recipe.</li>
-              <li>I was fully responsible for all aspects of the application down to styling, as well as API calls from the third-party API</li>
-              <li>Technologies Used: React, JavaScript, Bootstrap</li>
-            </ul>
-          </div>
-          <div>
-            <p className='resume_p_title'>The Collective: <a href='https://github.com/DXHASE/Collective-Social-App'>https://github.com/DXHASE/Collective-Social-App</a> | <a href='https://social-collective-app.herokuapp.com/'>https://social-collective-app.herokuapp.com/</a></p>
+            <p className='resume_p_title'>The Collective: <a href='https://github.com/DXHASE/Collective-Social-App' className='resume_link'>GitHub Repository</a> | <a href='https://social-collective-app.herokuapp.com/' className='resume_link'>Deployed Application</a></p>
             <ul>
               <li>A social media platform that allows users to log in or sign up. Once the user is logged in they will be able to view past posts by other users and visit their own profile via the nav bar where they will be able to create, update, and delete their own posts.</li>
               <li>front end and backend communication via API calls as well as assist with styling</li>
