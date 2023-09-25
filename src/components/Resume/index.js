7d49159e44cd8d926a0d9a32a6c2b5628f824ff9
chaz Graham <chazhg80.cg@gmail.com>
2023-09-25 12:10:25 -0500

    resturctured resume page to reflect new resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -16,71 +16,122 @@ function Resume() {
       </section>
       <Container>
         <section>
+          <h3>Chaz Graham</h3>
           <div className='resume_part1'>
-            <h3>Chaz Graham</h3>
-            <p className='resume_p_title'>San Antonio, TX</p>
-            <p className='resume_p_title'>Email: <a href="mailto: chazhg80.cg@gmail.com" className='resume_link'>chazhg80.cg@gmail.com</a></p>
-            <p className='resume_p_title'>Linkedin: <a href='https://www.linkedin.com/in/chaz-graham-a95a43258' className='resume_link'>www.linkedin.com/in/chaz-graham-a95a43258</a></p>
-            <p className='resume_p_title'>Portfolio: <a href='http://chazgraham.github.io/Chazs-React-Portfolio' className='resume_link'>http://chazgraham.github.io/Chazs-React-Portfolio</a></p>
-            <p className='resume_p_title'>GitHub: <a href='https://github.com/chazgrahamChaz' className='resume_link'>https://github.com/chazgrahamChaz</a></p>
+            <p className='resume_p_title'><a href="mailto: chazhg80.cg@gmail.com" className='resume_link'>chazhg80.cg@gmail.com</a> • </p>
+            <p className='resume_p_title'><a href='https://www.linkedin.com/in/chaz-graham-a95a43258' className='resume_link'> Linkedin</a> • </p>
+            <p className='resume_p_title'><a href='https://github.com/chazgrahamChaz' className='resume_link'> GitHub</a> • </p>
+            <p className='resume_p_title'><a href='http://chazgraham.github.io/Chazs-React-Portfolio' className='resume_link'> Portfolio</a></p>
           </div>
           <div>
-            <h3>Summary</h3>
-            <p className='resume_p'>Full Stack Web Developer Effective at combining creativity and problem solving to develop user-friendly applications that are responsive and compelling. Experience working with APIs, either RESTful or GraphQL-based, using Javascript. Self-starting learner driven by curiosity paired with solid GitHub skills. I have a meticulous approach to my work with an excellent attention to detail. I am willing to travel for new roles.</p>
+            <h3>Full Stack Engineer | Software Engineer | Frontend Engineer</h3>
+            <p className='resume_about'>
+              Bootcamp certificate holder, multilingual software engineer with a broad set of technical skills including design,
+              testing, and development. Love for learning new technologies and a passion for clean code.
+            </p>
           </div>
           <div>
-            <h3>Skills</h3>
-            <p className='resume_p'>Technical Languages/Skills:  HTML/HTML5, JavaScript, React.js, jQuery, Express, Apollo, MongoDB, GraphQl, Bcrypt, WordPress</p>
-            <p className='resume_p'>Soft Skills: Communication, Teamwork, Problem-solving, Flexibility, Adaptability.</p>
-            <p className='resume_p'>Applications: Full Stack Web Development and WordPress</p>
+            <h4 className='left'>Skills</h4>
+            <p className='resume_p'><strong>Programming: </strong>JavaScript, Node.js, HTML5, CSS</p>
+            <p className='resume_p'><strong>Libraries/Platforms: </strong>Express, React, jQuery</p>
+            <p className='resume_p'><strong>Data/Databases: </strong>SQL, NoSQL, MySQL, MongoDb</p>
+            <p className='resume_p'><strong>Cloud/Technologies: </strong>Heroku, Stripe</p>
           </div>
         </section>
         <section>
-          <h3>Relative Experience</h3>
+          <h4 className='left'>Relative Experience</h4>
           <div>
-            <p className='resume_p_title'>Arcadium: <a href='https://github.com/chazgraham/Arcadium' className='resume_link'>GitHub Repository</a> | <a href='http://chazgraham.github.io/Arcadium' className='resume_link'>Deployed Application</a></p>
+            <div className='project_title'>
+              <p className='resume_p_title2'>Arcadium</p>
+              <a href='https://github.com/chazgraham/Arcadium' className='resume_link'>GitHub Repository</a>
+              <p>Stack: Javascript, React, CSS</p>
+              <p>2023</p>
+            </div>
             <ul>
-              <li>An application that allows the user to choose a game that is posted on the homepage and allow them to play the game and view their score. All logic coded in JavaScript.</li>
-              <li>This application cures boredom and allows the user to relax by playing simple games.</li>
-              <li>Technologies Used: React, JavaScript, CSS</li>
+              <li>Developed a gaming library using Javascript and React, letting users choose a game of their choice. This helps destress and allows users to recharge.</li>
+              <li>Defined 3 games- candy smasher, tic-tac-toe, and rock paper scissors, which increased variety and gave users options depending on their play preference.</li>
             </ul>
           </div>
           <div>
-            <p className='resume_p_title'>Game Seeker: <a href='https://github.com/chazgraham/Game-Seeker' className='resume_link'>GitHub Repository</a> | <a href='https://game-seeker.herokuapp.com/' className='resume_link'>Deployed Application</a></p>
+            <div className='project_title'>
+              <p className='resume_p_title2'>Foodies</p>
+              <a href='http://chazgraham.github.io/Foodies' className='resume_link'>GitHub Repository</a>
+              <p>Stack: Javascript, React, Bootstrap</p>
+              <p>2023</p>
+            </div>
             <ul>
-              <li>The application allows users to track games they have played, are playing, and have completed. It also allows the user to make friends and view the same list on their profiles.</li>
-              <li>I was fully responsible for all aspects of the application down to styling and database creation as well as API calls from front-end to back-end. Used effective time management skills to meet my deadline.</li>
-              <li>Technologies Used: React, JavaScript, Apollo, MongoDb, GraphQl, Express, Bcrypt</li>
+              <li>Created an application that assists users in cooking using 3rd party API calls to the spoonacular recipe database. This gives users thousands of new recipes to try.</li>
+              <li>Using the React platform, I manipulated the data to display the necessary steps as well as a list of ingredients to recreate the recipe, allowing users to create dishes they have never attempted.</li>
             </ul>
           </div>
           <div>
-            <p className='resume_p_title'>Foodies: <a href='http://chazgraham.github.io/Foodies' className='resume_link'>GitHub Repository</a> | <a href='https://github.com/chazgraham/Foodies' className='resume_link'>Deployed Application</a></p>
+            <div className='project_title'>
+              <p className='resume_p_title2'>Game Seeker</p>
+              <a href='https://github.com/chazgraham/Game-Seeker' className='resume_link'>GitHub Repository</a>
+              <p>Stack: Javascript, React, MongoDb, Node.js</p>
+              <p>2022</p>
+            </div>
             <ul>
               <li>This application allows the user to search for their desired dish they would like to make and will give a short summary and provide the user will simple steps to recreate the dish.</li>
               <li>Solely contributed to design, styling, and API integration with efficient time management</li>
               <li>Technologies Used: JavaScript, React.js, Bootstrap</li>
             </ul>
           </div>
-        </section>
-        <section>
-          <h3>Experience</h3>
-          <p className='resume_p_title'>United States Air Force</p>
-          <p className='resume_p_title'>Aerospace Medical Technician / SSgt - From years 2015-2021</p>
-          <p className='resume_p'>Led night shift operations managing 20 thousand appointments with a small team of 20 medical members. I have strong leadership and following qualities to fit the role I am placed in. Quick picking up new skills and applying them into the workplace. Key Accomplishments:</p>
-          <ul>
-            <li>Oversaw admin medical training program trained 20 medics on 65 critical items that lowered documentation errors by 30%</li>
-            <li>Facilitated emergency vehicle rescue, coordinated with 3 agencies secured 70k in assets</li>
-          </ul>
+          <div>
+            <div className='project_title'>
+              <p className='resume_p_title2'>Express Shop</p>
+              <a href='https://github.com/chazgraham/Express-shop' className='resume_link'>GitHub Repository</a>
+              <p>Stack: Javascript, React, MongoDb, Node.js</p>
+              <p>2022</p>
+            </div>
+            <ul>
+              <li> Programmed an ecommerce application, using object data modeling (mongoose) to query the database, allows users to add/remove items from their cart</li>
+              <li>Implemented stripe checkout, giving users secure checkout with a custom order recipient.</li>
+            </ul>
+          </div>
+          <div>
+            <div className='project_title'>
+              <p className='resume_p_title2'>Music Playlist</p>
+              <a href='https://github.com/chazgraham/Music--Playlist' className='resume_link'>GitHub Repository</a>
+              <p>Stack: Javascript, HTML, Bulma</p>
+              <p>2022</p>
+            </div>
+            <ul>
+              <li>Led application design in a team of 3 developers. Created an effective and interactive web-based music playlist creator using 3rd party API calls to mixcloud database.</li>
+              <li>Updated application with 4 new features, giving users visual thumbnails, song names, ability to save songs to playlist, and display saved playlist. Enhanced user experience with individualistic playlist.</li>
+            </ul>
+          </div>
+          <div>
+            <div className='project_title'>
+              <p className='resume_p_title2'>JavaScript Test</p>
+              <a href='https://github.com/chazgraham/Chazs-JavaScript-Knowledge-Test' className='resume_link'>GitHub Repository</a>
+              <p>Stack: Javascript, HTML, CSS</p>
+              <p>2022</p>
+            </div>
+            <ul>
+              <li>Built a knowledge testing application using javascript, testing the users knowledge and proficiency with the javascript language. Rates users knowledge on a 1-10 scale in basic javascript knowledge.</li>
+              <li>Using set-timer functionality, the application tests the user in a timed environment. Allows users to gauge their speed of memory recall.</li>
+            </ul>
+          </div>
+          <div>
+            <div className='project_title'>
+              <p className='resume_p_title2'>Socal Collective</p>
+              <a href='https://github.com/DXHASE/Collective-Social-App' className='resume_link'>GitHub Repository</a>
+              <p>Stack: Javascript, HTML, SQL</p>
+              <p>2022</p>
+            </div>
+            <ul>
+              <li>Co-developed on a team of 3 developers. Developed an interactive social media platform, allowing users to sign up, post, like, and comment on images with captions</li>
+              <li>Defined database schemas and wrote efficient retrieval queries in SQLl for various user requirements.</li>
+              <li>Implemented templating language (handlebars.js), simplified work load and decreased total lines of code.</li>
+            </ul>
+          </div>
         </section>
         <section>
           <h3>Education</h3>
           <div>
             <p className='resume_p_title'>Boot Camp Certificate: University of Texas at San Antonio, San Antonio, TX</p>
             <p className='resume_p'>A 24-week intensive program focused on gaining technical programming skills in HTML5, CSS, Javascript, JQuery, Bootstrap, IndexDB, Node.js, MySQL, MongoDB, Express, Handelbars.js, and ReactJS.</p>
-          </div>
-          <div>
-            <p className='resume_p_title'>Basic Medical Technician Corpsman Program: Community College of the Air Force, San Antonio, TX</p>
-            <p className='resume_p'>A 24-week intensive program focused on gaining technical skills in emergency medicine.</p>
           </div>
         </section>
       </Container>
